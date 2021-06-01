f6350d39849dd9889925b3dffd84247d4c80b30e
EMİRHAN AY <dev.emirhanay@gmail.com>
2021-06-02 01:24:43 +0300

    Result system added

---
+++ northwind/src/main/java/kodlamaio/northwind/core/utulities/results/Result.java
@@ -0,0 +1,23 @@
+package kodlamaio.northwind.core.utulities.results;
+
+public class Result {
+	private boolean success;
+	private String message;
+
+	public Result(boolean success, String message) {
+		this(success);
+		this.message = message;
+	}
+
+	public Result(boolean success) {
+		this.success = success;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
---
+++ northwind/src/main/java/kodlamaio/northwind/core/utulities/results/SuccessResult.java
@@ -0,0 +1,12 @@
+package kodlamaio.northwind.core.utulities.results;
+
+public class SuccessResult extends Result{
+
+	public SuccessResult() {
+		super(true);
+	}
+	
+	public SuccessResult(String message) {
+		super(true, message);
+	}
+}
