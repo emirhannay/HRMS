b1c3b3e5f8527863d82aedf40712725a5822b451
EMİRHAN AY <dev.emirhanay@gmail.com>
2021-06-02 18:31:26 +0300

    Added api required for the system.

---
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/JobSeekerController.java
@@ -0,0 +1,42 @@
+package kodlamaio.northwind.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.northwind.business.abstracts.JobSeekerService;
+import kodlamaio.northwind.core.utulities.results.DataResult;
+import kodlamaio.northwind.core.utulities.results.Result;
+import kodlamaio.northwind.entity.concretes.JobSeeker;
+
+@RestController
+@RequestMapping(path = "api/job-seekers")
+public class JobSeekerController {
+	private JobSeekerService jobSeekerService;
+	
+	@Autowired
+	public JobSeekerController(JobSeekerService jobSeekerService) {
+		super();
+		this.jobSeekerService = jobSeekerService;
+	}
+	@GetMapping("/getall")
+	public 		DataResult<List<JobSeeker>> getAll(){
+		return jobSeekerService.getAll();
+	};
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody JobSeeker jobSeeker) {
+		return jobSeekerService.add(jobSeeker);
+	};
+	
+	
+	
+	
+	
+
+}
