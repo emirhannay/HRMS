5a0aececd174bf071d8cd567bb2b259ba261575e
EMİRHAN AY <dev.emirhanay@gmail.com>
2021-06-12 23:05:08 +0300

    Necessary relationships have been established for the CV of Job Seekers.

--- northwind/src/main/java/kodlamaio/northwind/entity/concretes/JobSeeker.java
+++ northwind/src/main/java/kodlamaio/northwind/entity/concretes/JobSeeker.java
@@ -3,13 +3,17 @@ package kodlamaio.northwind.entity.concretes;
 
 
 import java.time.LocalDate;
-
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import kodlamaio.northwind.entity.abstracts.User;
 import lombok.AllArgsConstructor;
@@ -25,26 +29,43 @@ import lombok.NoArgsConstructor;
 public class JobSeeker extends User {
 
 	
-    public JobSeeker(String firstName, String lastName, String identificationNo, LocalDate birthDate, String email, String password)
-    {
-        super(email,password);
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.identificationNo=identificationNo;
-        this.birthDate=birthDate;
-    }
+
 	
+    
 	@Column(name = "first_name")
 	private String firstName;
 	@Column(name = "last_name")
 	private String lastName;
 	@Column(name = "national_id")
-	private String identificationNo;
+	private String nationalId;
 	@Column(name = "date_of_birth")
 	private LocalDate birthDate;
-	@Column(name ="is_verified")
+	@Column(name ="is_verified", columnDefinition = "boolean default false")
 	private boolean isVerified;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "jobSeeker")
+	private List<SchoolForCV> schools;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "jobSeeker")
+	private List<ProgrammingSkillForCV> programingSkills;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "jobSeeker")
+	private List<ForeignLanguageForCV> languages;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "jobSeeker")
+	private List<ExperienceForCV> experiences;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "jobSeeker")
+	private List<CoverLetterForCV> coverLetters;
+
+	@JsonIgnore
+	@OneToOne(mappedBy = "jobSeeker", optional = false, fetch=FetchType.LAZY)
+	private ImageForCV image;
 
 	
 
